5b53b7ff17ccbbf426d2c2eba2f48ed807ec602e
Hannes Lindquist <hannes.lindquist@gmail.com>
2018-12-09 22:45:19 +0100

    chore: fix right git subtree prefix

--- publish.sh
+++ publish.sh
@@ -5,7 +5,7 @@ git branch -D gh-pages
 npm run build
 git add build -f
 git commit -m "publish gh-pages"
-git subtree split --prefix output -b gh-pages
+git subtree split --prefix build -b gh-pages
 git push -f origin gh-pages:gh-pages
 git fetch origin
 git branch -D gh-pages
